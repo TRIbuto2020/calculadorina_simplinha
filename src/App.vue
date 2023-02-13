c2accb1f4db1c62965e45230c6cf2feb45caa380
Vitor RS <vitor2000rs@gmail.com>
2023-02-13 08:54:49 -0300

    Neubrutalismo

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
 
 <template>
   <div>
-    <h1>Calculadorinha Simplinha</h1>
+    <h1>Calcule!</h1>
     <form>
       <input type="number" :value="estado.itemUm" @change="evento => estado.itemUm = evento.target.value" @keyup="evento => estado.itemUm = evento.target.value" min="0">
       <select @change="evento => estado.operacao = evento.target.value">
@@ -41,53 +41,4 @@
 </template>
 
 <style scoped>
-  *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: sans-serif;
-    color: #ddd;
-  }
-
-  div{
-    width: 900px;
-    height: 100vh;
-    max-width:80%;
-    margin: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-
-  h1{
-    margin-bottom: 48px;
-    font-size: 40px;
-    text-align: center;
-  }
-
-  form{
-    margin: 0 auto;
-    display: flex;
-    gap: 10%;
-  }
-  
-  select{
-    width: 20%;
-  }
-  
-  input, select{
-    text-align: center;
-    font-size: 28px;
-    width: 40%;
-    background-color: #333;
-    padding: 8px;
-    border: #ddd solid 1px;
-  }
-
-  h2{
-    margin-top: 40px;
-    font-size: 36px;
-  }
-
 </style>
